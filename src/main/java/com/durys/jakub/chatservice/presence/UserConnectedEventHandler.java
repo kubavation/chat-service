1fe1fe4bc93ebb58c890064d867286a5ba3a7c91
kubavation <37585627+kubavation@users.noreply.github.com>
2024-02-28 20:50:02 +0100

    usage of presenceService

--- src/main/java/com/durys/jakub/chatservice/presence/UserConnectedEventHandler.java
+++ src/main/java/com/durys/jakub/chatservice/presence/UserConnectedEventHandler.java
@@ -1,5 +1,6 @@
 package com.durys.jakub.chatservice.presence;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
@@ -8,14 +9,22 @@ import org.springframework.web.socket.messaging.SessionConnectEvent;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 class UserConnectedEventHandler {
 
     private static final String USER_ID = "user-id";
 
+    private final PresenceService presenceService;
+
     @EventListener
     public void handleSessionConnected(SessionConnectEvent event) {
 
-        log.info("{} connected", retrieveUserId(event));
+        Long userId = Long.valueOf(retrieveUserId(event));
+
+        log.info("{} connected", userId);
+
+        presenceService.notifyAboutPresence(userId);
+
     }
 
     private static String retrieveUserId(SessionConnectEvent event) {
