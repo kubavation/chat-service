9ad8a3e2dda545c412cc010478334661f4ca0600
kubavation <37585627+kubavation@users.noreply.github.com>
2024-03-04 20:37:54 +0100

    presence repo

--- src/main/java/com/durys/jakub/chatservice/presence/PresenceRepository.java
+++ src/main/java/com/durys/jakub/chatservice/presence/PresenceRepository.java
@@ -1,6 +1,12 @@
-package com.durys.jakub.chatservice.participant;
+package com.durys.jakub.chatservice.presence;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.data.cassandra.repository.Query;
 
-public interface ParticipantRepository extends CassandraRepository<Participant, Long> {
+import java.util.List;
+
+public interface PresenceRepository extends CassandraRepository<Presence, Long> {
+
+    @Query("select * from presence where userid = :userId")
+    List<Presence> findAllByUserId(Long userId);
 }
