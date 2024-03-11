bcd07274cebe04051983fba1bd82880fb536ae3b
kubavation <37585627+kubavation@users.noreply.github.com>
2024-03-11 20:01:55 +0100

    changes in WsURI

--- src/main/java/com/durys/jakub/chatservice/ws/WsURI.java
+++ src/main/java/com/durys/jakub/chatservice/ws/WsURI.java
@@ -1,16 +1,22 @@
 package com.durys.jakub.chatservice.ws;
 
 public enum WsURI {
-    CHAT("/topic/messages/%d"),
-    PRESENCE("/topic/presence/%d");
+    CHAT("/topic/messages/%d", "/topic/messages"),
+    PRESENCE("/topic/presence/%d", "/topic/presence");
 
     private final String uri;
+    private final String endpoint;
 
-    WsURI(String uri) {
+    WsURI(String uri, String endpoint) {
         this.uri = uri;
+        this.endpoint = endpoint;
     }
 
     public String uri() {
         return uri;
+    }
+
+    public String endpoint() {
+        return endpoint;
     }
 }
